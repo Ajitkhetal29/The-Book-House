7535cc2d286e43948e1530602bc81569ed4b1454
iamajitkhetal <114768969+iamajitkhetal@users.noreply.github.com>
2025-08-02 02:18:28 +0530

    thrd commit

--- server/server.js
+++ server/server.js
@@ -14,9 +14,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/',(req, res)=>{
-  res.send("Hello from The Book House ")
-})
 
 app.use("/api/user/", userRouter);
 app.use("/api/book/", bookRouter);
