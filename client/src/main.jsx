689622912e4809d448619ef72ab696385d968ebe
iamajitkhetal <114768969+iamajitkhetal@users.noreply.github.com>
2025-08-05 13:48:51 +0530

    third commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import {  HashRouter,BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <AppContextProvider>
       <App />
     </AppContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
